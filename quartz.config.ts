29fa2baa472dc7fec6c8aed3579e9e299b1f5f89
Watrre <thewatrre@gmail.com>
2024-04-05 05:56:01 -0400

    Quartz sync: Apr 5, 2024, 5:56 AM

--- quartz.config.ts
+++ quartz.config.ts
@@ -15,7 +15,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "watrre.github.io/AoTSB",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "created",
     theme: {
